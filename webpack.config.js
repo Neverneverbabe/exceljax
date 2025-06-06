51f5b1d0183f66c8b1946ecb04507aaf90ec5400
Neverneverbabe <128009213+Neverneverbabe@users.noreply.github.com>
2025-06-06 13:22:13 -0600

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,45 @@ module.exports = {
     taskpane: './src/taskpane.js',
   },
   output: {
+    path: path.resolve(__dirname, 'const path = require('path');
+
+module.exports = {
+  entry: {
+    taskpane: './src/taskpane/taskpane.js',
+  },
+  output: {
     path: path.resolve(__dirname, 'dist'),
+    filename: '[name].js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name].[ext]',
+            },
+          },
+        ],
+      },
+      {
+        test: /\.css$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name].[ext]',
+            },
+          },
+        ],
+      }
+    ],
+  },
+  mode: 'production',
+  devtool: 'source-map',
+};dist'),
     filename: '[name].js',
   },
   module: {
