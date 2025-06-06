4121ccdae5e7acec730805d363cb366c28f93d74
Neverneverbabe <128009213+Neverneverbabe@users.noreply.github.com>
2025-06-06 23:04:52 +0000

    fix: use HtmlWebpackPlugin to emit taskpane.html

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,5 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
   entry: {
@@ -10,20 +9,15 @@ module.exports = {
     path: path.resolve(__dirname, 'dist'),
     filename: '[name].js',
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      filename: 'taskpane.html',
+      template: './src/taskpane/taskpane.html',
+      chunks: ['taskpane'],
+    }),
+  ],
   module: {
     rules: [
-      {
-        test: /\.html$/,
-        use: [
-          {
-            loader: 'file-loader',
-            options: {
-              name: '[name].[ext]',
-            },
-          },
-        ],
-        exclude: /node_modules/,
-      },
       {
         test: /\.css$/,
         use: [
@@ -34,23 +28,9 @@ module.exports = {
             },
           },
         ],
-        exclude: /node_modules/,
-      }
+      },
     ],
   },
-  plugins: [
-    new HtmlWebpackPlugin({
-      filename: 'taskpane.html',
-      template: './src/taskpane/taskpane.html',
-      chunks: ['taskpane'],
-    }),
-    new CopyWebpackPlugin({
-      patterns: [
-        { from: 'manifest.xml', to: '.' },
-        { from: 'assets', to: 'assets' }
-      ]
-    })
-  ],
   mode: 'production',
   devtool: 'source-map',
 };
