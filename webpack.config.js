0d8193a80017c4c4f32962d0e18ba8eacee06912
Neverneverbabe <128009213+Neverneverbabe@users.noreply.github.com>
2025-06-06 00:18:29 -0600

    Update production URL for GitHub Pages

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ const CopyWebpackPlugin = require("copy-webpack-plugin");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 const urlDev = "https://localhost:3000/";
-const urlProd = "https://www.contoso.com/"; // CHANGE THIS TO YOUR PRODUCTION DEPLOYMENT LOCATION
+const urlProd = "https://neverneverbabe.github.io/exceljax/"; // Updated for GitHub Pages
 
 async function getHttpsOptions() {
   const httpsOptions = await devCerts.getHttpsServerOptions();
